93e5ccfa8fc9b7790095f3a3f10a3961f68137ad
athiragithubworld <athirahrms@gmail.com>
2024-07-01 23:31:51 +0530

    Server Created

--- node.js
+++ node.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+
+const product = () => 1 * 3;
+
+console.log(product());
+
+const student = {
+  name: "Athi",
+  age: 15,
+
+  // here this won't work
+  greet1: () => {
+    console.log("Hello , I'm " + this.name);
+  },
+  
+  // this will work
+  greet() {
+    console.log("Hello , I'm " + this.name);
+  },
+  greet12: function () {
+    console.log("Hi , I'm " + this.age + "years old");
+  },
+};
+
+student.greet()
+student.greet12();
+
+const array = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon']
+console.log(array.map(fruit => fruit ==' '?'empty string':fruit))
